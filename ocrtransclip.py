49b74d1a39cdf0db185ed2f84bdda98ba78fd75b
y-tetsu <45305605+y-tetsu@users.noreply.github.com>
2021-07-25 19:56:22 +0900

    add main application

--- ocrtransclip.py
+++ ocrtransclip.py
@@ -1,13 +1,46 @@
 """ocrtransclip
 """
 import os
+import time
 import pathlib
 import re
 
-from PIL import Image
+import numpy as np
+from PIL import Image, ImageGrab
 import pyocr
 import googletrans
 import pyperclip
+
+
+class OcrTransClip:
+    def __init__(self):
+        self.ocrtool = OcrTool('C:\\Program Files\\Tesseract-OCR')
+        self.translator = Translator()
+
+    def run(self):
+        print('[ocrtransclip]\n * Please capture desktop by pressing Win+Shift+S. ')
+        pre_clip_image = ImageGrab.grabclipboard()
+        while True:
+            try:
+                clip_image = ImageGrab.grabclipboard()
+            except OSError:
+                pass
+            if not np.array_equal(np.array(clip_image), np.array(pre_clip_image)):
+                if isinstance(clip_image, Image.Image):
+                    # ----------
+                    print('\n--- OCR start... ---')
+                    ocr_text = self.ocrtool.image_to_string(clip_image)
+                    print(ocr_text)
+                    print('\n--- translation start... ---')
+                    trans_text = self.translator.run(ocr_text)
+                    print(trans_text)
+                    print('\n--- send to clipboard... ---')
+                    pyperclip.copy(trans_text)
+                    print('done')
+                    # ----------
+                    pre_clip_image = clip_image
+                    time.sleep(1.0)
+            time.sleep(0.3)
 
 
 class OcrTool:
@@ -29,7 +62,10 @@ class OcrTool:
     def _setup_tesseract_builder(self, layout):
         return pyocr.builders.TextBuilder(tesseract_layout=layout)
 
-    def image_to_string(self, image_file):
+    def image_to_string(self, img):
+        return self.tool.image_to_string(img, lang=self.lang, builder=self.builder)
+
+    def imagefile_to_string(self, image_file):
         img = Image.open(image_file)
         return self.tool.image_to_string(img, lang=self.lang, builder=self.builder)
 
@@ -55,38 +91,40 @@ class Translator:
         src, dest = ('ja', 'en') if detect.lang == 'ja' else ('en', 'ja')
         text = self._pre_format(text, src, dest)
         translated = self.translator.translate(text, src=src, dest=dest).text
-        pyperclip.copy(translated)
         return translated
 
 
 if __name__ == '__main__':
-    ocrtool = OcrTool('C:\\Program Files\\Tesseract-OCR')
-    translator = Translator()
+    tool = OcrTransClip()
+    tool.run()
 
-    # test01
-    path = pathlib.Path('.\\tests\\test01.png')
-
-    # OCR
-    ocr_text = ocrtool.image_to_string(str(path))
-    expected = """Home
-
-stefan Weiledited this page 26 days ago・70 revisions
-
-Tesseract at UB Mannheim
-
-The Mannheim University Librar (UB Mannheim) uses Tesseract to perform OCR (optical character recognition) of historical
-German newspapers (Allgemeine PreuBische Staatszeituno, Deutscher Reichsanzeige. The latest results with OCR from more
-
-than 360,000 scans are available online."""
-    print(ocr_text)
-    assert ocr_text == expected, '*** Error : ocr_text is not match. ***\n(expected)\n---\n' + expected + '\n---\n'
-
-    # translation
-    trans_text = translator.run(ocr_text)
-    expected = """家
-Stefan weilileedこのページ26日前・70リビジョン
-UBマンハイムのテッセサクト
-Mannheim University Librar（UB Manheim）はTesseractを使用して歴史的なドイツの新聞のOCR（Optical Character認識）を実行します（Allgemeine Preubische Staatszeituno、Deutscher Reichsanzeige。
-360,000以上のスキャンからOCRを持つ最新の結果がオンラインで入手可能です。"""
-    print(trans_text)
-    assert trans_text == expected, '*** Error : trans_text is not match. ***\n(expected)\n---\n' + expected + '\n---\n'
+#     ocrtool = OcrTool('C:\\Program Files\\Tesseract-OCR')
+#     translator = Translator()
+#
+#     # test01
+#     path = pathlib.Path('.\\tests\\test01.png')
+#
+#     # OCR
+#     ocr_text = ocrtool.imagefile_to_string(str(path))
+#     expected = """Home
+#
+# stefan Weiledited this page 26 days ago・70 revisions
+#
+# Tesseract at UB Mannheim
+#
+# The Mannheim University Librar (UB Mannheim) uses Tesseract to perform OCR (optical character recognition) of historical
+# German newspapers (Allgemeine PreuBische Staatszeituno, Deutscher Reichsanzeige. The latest results with OCR from more
+#
+# than 360,000 scans are available online."""
+#     print(ocr_text)
+#     assert ocr_text == expected, '*** Error : ocr_text is not match. ***\n(expected)\n---\n' + expected + '\n---\n'
+#
+#     # translation
+#     trans_text = translator.run(ocr_text)
+#     expected = """家
+# Stefan weilileedこのページ26日前・70リビジョン
+# UBマンハイムのテッセサクト
+# Mannheim University Librar（UB Manheim）はTesseractを使用して歴史的なドイツの新聞のOCR（Optical Character認識）を実行します（Allgemeine Preubische Staatszeituno、Deutscher Reichsanzeige。
+# 360,000以上のスキャンからOCRを持つ最新の結果がオンラインで入手可能です。"""
+#     print(trans_text)
+#     assert trans_text == expected, '*** Error : trans_text is not match. ***\n(expected)\n---\n' + expected + '\n---\n'
